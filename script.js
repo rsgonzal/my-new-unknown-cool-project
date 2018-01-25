61475ea322b1ddff069bb28b1aadf42c20bf7611
Roxana <roxana@iridescentlearning.org>
2018-01-24 20:32:59 -0800

    simplified app

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 // Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName("LI");
-let i;
-for (i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+// let myNodelist = document.getElementsByTagName("LI");
+// let i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   let span = document.createElement("SPAN");
+//   let txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
 
-// Click on a close button to hide the current list item
+// // Click on a close button to strike-through the current list item
 let close = document.getElementsByClassName("close");
- i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let div = this.parentElement;
-    div.classList.toggle('line-through');
-  }
-}
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     let div = this.parentElement;
+//     div.classList.toggle('line-through');
+//   }
+// }
 
 // Add a "checked" symbol when clicking on a list item
 let list = document.querySelector('ul');
